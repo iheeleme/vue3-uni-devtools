11078cc7f679fd0667bce7cddfcd184ede0b38eb
seesawz <545243476@qq.com>
2024-01-17 23:14:06 +0800

    feat: 添加打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build:{
+    //库模式
+    lib:{
+      entry:resolve(__dirname,'src/main.ts'),
+      name:'vue3-uni-devtools',
+      fileName: 'vue3-uni-devtools',
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['vue'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    }
+  }
 })
